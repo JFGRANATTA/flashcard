8a705eec4d9c321c0f4938bfa512b687291cf12c
JFGRANATTA <140609024+JFGRANATTA@users.noreply.github.com>
2024-10-29 08:46:23 -0300

    29/10

---
+++ perguntas.js
@@ -0,0 +1,50 @@
+criaCartao(
+    'História',
+    'Em que ano aconteceu a Primeira Guerra Mundial?',
+    'De 1914 a 1918'
+)
+criaCartao(
+  'História',
+  'Em que país se iniciou a Revolução Industrial?',
+  'Na Inglaterra'
+)
+criaCartao(
+   'Português',
+   'A palavra "volúpia" está relacionada',
+   'Doença'
+)
+criaCartao(
+    'Biologia',
+    'O que é recombinação gênica?',
+    'Mistura de genes entre indivíduos da mesma espécie'
+)
+criaCartao(
+   'Biologia',
+   'Quem propôs a seleção natural?',
+   'Charles Darwin'
+)
+criaCartao(
+   'Geografia',
+   'O território brasileiro possui quantas regiões? E quais são elas?',
+   '5 regiões, sendo Norte, Nordeste, Centro-oeste, Sudeste e Sul'
+)
+criaCartao(
+    'Sociologia',
+    'O que é estratificação social?',
+    'Divisão da sociedade em camadas'
+)
+criaCartao(
+  'História',
+  'Em qual governo foi consolidado a CLT?',
+  'No governo/ era Vargas'  
+)
+criaCartao(
+    'Química',
+    'O que acontece no processo de oxidação?',
+    'Perda de elétrons e aumento do NOX.'
+)
+criaCartao(
+    'Português',
+    'Leniência é uma palavra que significa:',
+    'Agradável'
+)
