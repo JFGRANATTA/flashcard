b42d4798208599f73bf442284ebd2ec321aff8a8
JFGRANATTA <140609024+JFGRANATTA@users.noreply.github.com>
2024-11-05 08:32:57 -0300

    x

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function criaCartao(categoria, pergunta, respota){
+function criaCartao(categoria, pergunta, resposta){
 let cartao = document.createElement('article')
 cartao.className = 'cartao'
 cartao.innerHTML = `
